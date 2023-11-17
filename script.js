0d6020ec3db382bdcbc34f90ab52743abaf4c58b
LeFredy <forgetfrederickgmail.com>
2023-11-17 11:39:08 -0500

    now it works

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function rotate_y(a) {
     return -Math.cos(a)
 }
 
+
+
 window.addEventListener("load", (event) => {
     
     console.log("has been called");
@@ -14,8 +16,11 @@ window.addEventListener("load", (event) => {
         y: 0.0,
         a: 0.0,
         screena: 0.0,
-        v: 200,
-        d: -1,
+        v: 300,
+        av: 3,
+        kright: 0,
+        kleft: 0,
+        camera_ease: 0.20
     };
     
     var canvas = document.querySelector("canvas#render");
@@ -28,6 +33,7 @@ window.addEventListener("load", (event) => {
     var images = [false];
     var deltatime = 0;
     var start;
+    var first = true;
     function image_pointer(n) {
         return (event) => {
             console.log(`image ${n} has been loaded`)
@@ -44,6 +50,20 @@ window.addEventListener("load", (event) => {
         alert("Canvas Graphics are not supported");
     }
 
+    function draw_at_r(x, y, a, func) {
+        ctx.save();
+        ctx.translate(x, y);
+        ctx.rotate(a);
+        func();
+        ctx.restore();
+    }
+    function draw_at(x, y, func) {
+        ctx.save();
+        ctx.translate(x, y);
+        func();
+        ctx.restore();
+    }
+
     function windowResizeEvent() {
         let w = window.innerWidth;
         let h = window.innerHeight;
@@ -57,6 +77,7 @@ window.addEventListener("load", (event) => {
 
     function loading(timeStamp) {
         if (images.every(Boolean)) {
+            ctx.save();
             clock();
         } else {
             requestAnimationFrame(loading);
@@ -64,59 +85,91 @@ window.addEventListener("load", (event) => {
     }
 
     function clock(timeStamp) {
-        if (start === undefined) {
-            start = timeStamp;
-        }
-        if (timeStamp != undefined) {
-        console.log("timeStamp " + timeStamp);
-        let ntime = (timeStamp - start) / 1000;
-        console.log("hey " + ntime);
-        deltatime = ntime - time;
-        time = ntime;
-        ctx.clearRect(0, 0, width, height);
-        console.log(deltatime);
-        console.log(time);
+        if (timeStamp) {
+            if (start === undefined) {
+                start = timeStamp;
+            }
+            if (first) {
+                first = false;
+            }
 
-        Player1.a += Player1.d * deltatime;
-        Player1.x += rotate_x(Player1.a) * Player1.v * deltatime;
-        Player1.y += rotate_y(Player1.a) * Player1.v * deltatime;
+            let ntime = (timeStamp - start) / 1000;
+            deltatime = ntime - time;
+            time = ntime;
+            ctx.clearRect(0, 0, width, height);
 
-        Player1.x = (Player1.x + 10240.0) % 20480 - 10240.0;
-        Player1.y = (Player1.y + 10240.0) % 20480 - 10240.0;
-        console.log(Player1);
-        draw();
-        
+            Player1.a += (Player1.kleft + Player1.kright) * Player1.av * deltatime;
+            Player1.screena += (Player1.a - Player1.screena) * Player1.camera_ease;
+            Player1.x += rotate_x(Player1.a) * Player1.v * deltatime;
+            Player1.y += rotate_y(Player1.a) * Player1.v * deltatime;
+
+            if (Player1.x > 5120) {
+                Player1.x = Player1.x - 10240
+            } else if (Player1.x < -5120) {
+                Player1.x = Player1.x + 10240
+            }
+            if (Player1.y > 5120) {
+                Player1.y = Player1.y - 10240
+            } else if (Player1.y < -5120) {
+                Player1.y = Player1.y + 10240
+            }
+            // Player1.x = (Player1.x - 5120) % 10240 + 5120;
+            // Player1.y = (Player1.y - 5120) % 10240 + 5120;
+            draw();
+            ++frameCount;
+            requestAnimationFrame(next_frame);
+        } else {
+            requestAnimationFrame(clock);
         }
-        requestAnimationFrame(clock);
-        ++frameCount;
+    }
+
+    function next_frame(timeStamp) {
+        clock(timeStamp);
     }
 
     function draw() {
         ctx.save();
-        ctx.translate(width / 2, height / 2);
-        ctx.save();
-        ctx.translate(-Player1.x, -Player1.y);
-        ctx.rotate(Player1.a);
-        const bg_pattern = ctx.createPattern(bg_tile, "repeat");
-        ctx.fillStyle = bg_pattern;
-        ctx.fillRect(-100000, -100000, 200000, 200000);
-        ctx.fillStyle = "rgb(0, 0, 200)";
-        ctx.fillRect(-20, -20, 40, 40);
-        ctx.restore();
-        ctx.fillStyle = "rgb(200, 0, 200)";
-        ctx.fillRect(-20, -20, 40, 40);
+        ctx.translate(width*0.5, height*0.5)
+        ctx.rotate(-Player1.screena);
+        ctx.translate(-Player1.x, -Player1.y)
+        // elements
+        draw_at(0, 0, () => {
+            let bg_pattern = ctx.createPattern(bg_tile, "repeat");
+            ctx.fillStyle = bg_pattern;
+            ctx.fillRect(-7000, -7000, 14000, 14000);
+        });
+        draw_at(0, 0, () => {
+            ctx.fillStyle = "rgb(0, 0, 200)";
+            ctx.fillRect(-20, -20, 40, 40);
+        });
+        draw_at_r(Player1.x, Player1.y, Player1.a, () => {
+            ctx.fillStyle = "rgb(200, 0, 200)";
+            ctx.fillRect(-10, -20, 20, 40);
+            ctx.fillRect(-20, 0, 40, 20);
+        });
 
+        
         ctx.restore();
-
         ctx.font = "16px monospace";
         ctx.strokeStyle = "#ffffff";
         ctx.strokeText(frameCount, 10, 10+16);
         ctx.font = "16px monospace";
         ctx.fillStyle = "#ffffff";
-        ctx.fillText(Math.round(time / 10) / 100, 10, 10+16+16);
+        ctx.fillText(Math.round(time * 100) / 100, 10, 10+16+16);
+        ctx.font = "16px monospace";
+        ctx.fillStyle = "#ffffff";
+        ctx.fillText(Math.round(Player1.x * 100) / 100, 10, 10+16+16+16);
+        ctx.font = "16px monospace";
+        ctx.fillStyle = "#ffffff";
+        ctx.fillText(Math.round(Player1.y * 100) / 100, 100, 10+16+16+16);
     }
-    document.body.addEventListener("keyup", () => {
-        Player1.d = -Player1.d
+    document.body.addEventListener("keyup", (event) => {
+        if (event.code === "ArrowLeft") {Player1.kleft = 0}
+        else if (event.code === "ArrowRight") {Player1.kright = 0}
+    });
+    document.body.addEventListener("keydown", (event) => {
+        if (event.code === "ArrowLeft") {Player1.kleft = -1}
+        else if (event.code === "ArrowRight") {Player1.kright = 1}
     });
 
     loading();
