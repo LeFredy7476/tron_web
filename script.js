2c9a10618e2e21228a9eb57c94deaf78d449faab
LeFredy <forgetfrederickgmail.com>
2023-11-17 16:17:33 -0500

    tout fonctionne et c'est optimisé

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+
+// quelques fonctions utiles pour fair des calculs
+
 function rotate_x(a) {
-    return Math.sin(a)
-}
-function rotate_y(a) {
-    return -Math.cos(a)
+    return Math.sin(a * Math.PI)
 }
 
+function rotate_y(a) {
+    return -Math.cos(a * Math.PI)
+}
+
+// algorithme permettant de rendre les mouvement de la caméra plus "smooth"
+
+function ease(number, target, fact, limit=0.002) {
+    if (Math.abs(target - number) < limit) {
+        return target;
+    } else {
+        return number + (target - number) * fact;
+    }
+}
 
 
 window.addEventListener("load", (event) => {
@@ -15,45 +28,61 @@ window.addEventListener("load", (event) => {
         x: 0.0,
         y: 0.0,
         a: 0.0,
-        screena: 0.0,
+        sx: 0.0,
+        sy: 0.0,
+        sa: 0.0,
         v: 300,
-        av: 3,
+        av: 1,
         kright: 0,
         kleft: 0,
-        camera_ease: 0.20
+        e: 0.10,
+        last: [0,0],
+        trace: [[[0,0], [0,0]]],
+        color: "#f0f"
     };
     
-    var canvas = document.querySelector("canvas#render");
     var width = 1600;
     var height = 900;
+    var mapsize = 1280;
+    var canvas = document.querySelector("canvas#render");
+    var mapcanvas = new OffscreenCanvas(width, height);
     var frameCount = 0;
     var time = 0;
     var start = undefined;
-    var ctx = null;
     var images = [false];
     var deltatime = 0;
     var start;
     var first = true;
+    var map_bitmap;
+    var pause = 0;
+
     function image_pointer(n) {
         return (event) => {
-            console.log(`image ${n} has been loaded`)
+            console.log(`image ${n} has been loaded`);
             images[n] = true;
         }
     }
+
     const bg_tile = new Image(100, 200);
-    bg_tile.onload = image_pointer(0)
+    bg_tile.onload = image_pointer(0);
     bg_tile.src = "bg_tile.png";
 
+    var ctx = null;
+    var mapctx = null;
     if (canvas.getContext) {
         ctx = canvas.getContext("2d");
+        mapctx = mapcanvas.getContext("2d", {
+            willReadFrequently: true
+        });
     } else {
         alert("Canvas Graphics are not supported");
     }
 
+
     function draw_at_r(x, y, a, func) {
         ctx.save();
         ctx.translate(x, y);
-        ctx.rotate(a);
+        ctx.rotate(a * Math.PI);
         func();
         ctx.restore();
     }
@@ -69,6 +98,8 @@ window.addEventListener("load", (event) => {
         let h = window.innerHeight;
         ctx.canvas.width = w;
         ctx.canvas.height = h;
+        mapctx.canvas.width = w;
+        mapctx.canvas.height = h;
         width = w;
         height = h;
     }
@@ -89,35 +120,60 @@ window.addEventListener("load", (event) => {
             if (start === undefined) {
                 start = timeStamp;
             }
-            if (first) {
-                first = false;
-            }
 
             let ntime = (timeStamp - start) / 1000;
             deltatime = ntime - time;
             time = ntime;
             ctx.clearRect(0, 0, width, height);
 
+            
+            
+
             Player1.a += (Player1.kleft + Player1.kright) * Player1.av * deltatime;
-            Player1.screena += (Player1.a - Player1.screena) * Player1.camera_ease;
+            Player1.sa = ease(Player1.sa, Player1.a, Player1.e);
             Player1.x += rotate_x(Player1.a) * Player1.v * deltatime;
             Player1.y += rotate_y(Player1.a) * Player1.v * deltatime;
+            if (Player1.kleft + Player1.kright === 0) {
+                Player1.trace[0].pop()
+            }
+            Player1.trace[0].push([Player1.x, Player1.y]);
+            Player1.last = [Player1.x, Player1.y];
 
-            if (Player1.x > 5120) {
-                Player1.x = Player1.x - 10240
-            } else if (Player1.x < -5120) {
-                Player1.x = Player1.x + 10240
+            if (frameCount > 1) {
+                map_bitmap = mapctx.getImageData(0, 0, width, height);
+
+                let hitX = Math.round(width*0.5) + Math.round(Player1.a - Player1.sa) * 100;
+                let hitY = Math.round(height*0.7) - 10;
+                let hitR = map_bitmap.data[hitY * map_bitmap.width * 4 + hitX * 4 + 0];
+                let hitG = map_bitmap.data[hitY * map_bitmap.width * 4 + hitX * 4 + 1];
+                let hitB = map_bitmap.data[hitY * map_bitmap.width * 4 + hitX * 4 + 2];
+                let hitA = map_bitmap.data[hitY * map_bitmap.width * 4 + hitX * 4 + 3];
+                console.log([hitR, hitG, hitB, hitA]);
+                if (hitB > 10) {
+                    Player1.color = "#f00";
+                }
             }
-            if (Player1.y > 5120) {
-                Player1.y = Player1.y - 10240
-            } else if (Player1.y < -5120) {
-                Player1.y = Player1.y + 10240
-            }
-            // Player1.x = (Player1.x - 5120) % 10240 + 5120;
-            // Player1.y = (Player1.y - 5120) % 10240 + 5120;
+
+            mapctx.clearRect(0, 0, width, height);
+
+
+            // if (Player1.x > mapsize) {
+            //     Player1.x = Player1.x - 2 * mapsize;
+            // } else if (Player1.x < -mapsize) {
+            //     Player1.x = Player1.x + 2 * mapsize;
+            // }
+            // if (Player1.y > mapsize) {
+            //     Player1.y = Player1.y - 2 * mapsize;
+            // } else if (Player1.y < -mapsize) {
+            //     Player1.y = Player1.y + 2 * mapsize;
+            // }
+
+            
             draw();
             ++frameCount;
-            requestAnimationFrame(next_frame);
+            if (pause === 0) {
+                requestAnimationFrame(next_frame);
+            }
         } else {
             requestAnimationFrame(clock);
         }
@@ -128,28 +184,70 @@ window.addEventListener("load", (event) => {
     }
 
     function draw() {
-        ctx.save();
-        ctx.translate(width*0.5, height*0.5)
-        ctx.rotate(-Player1.screena);
-        ctx.translate(-Player1.x, -Player1.y)
-        // elements
-        draw_at(0, 0, () => {
-            let bg_pattern = ctx.createPattern(bg_tile, "repeat");
-            ctx.fillStyle = bg_pattern;
-            ctx.fillRect(-7000, -7000, 14000, 14000);
-        });
-        draw_at(0, 0, () => {
-            ctx.fillStyle = "rgb(0, 0, 200)";
-            ctx.fillRect(-20, -20, 40, 40);
-        });
-        draw_at_r(Player1.x, Player1.y, Player1.a, () => {
-            ctx.fillStyle = "rgb(200, 0, 200)";
-            ctx.fillRect(-10, -20, 20, 40);
-            ctx.fillRect(-20, 0, 40, 20);
-        });
 
+        mapctx.save();
+        mapctx.translate(width*0.5, height*0.7)
+        mapctx.rotate(-Player1.sa * Math.PI);
+        mapctx.translate(-Player1.x, -Player1.y)
+        mapctx.fillStyle = "#fff";
+        mapctx.fillRect(-mapsize - 8, -mapsize - 8, mapsize * 2 + 16, mapsize * 2 + 16);
+        mapctx.fillStyle = "#000";
+        mapctx.fillRect(-mapsize, -mapsize, mapsize*2, mapsize*2);
+        mapctx.lineWidth = 16;
+        mapctx.lineCap = "round";
+        mapctx.lineJoin = "round";
+        mapctx.strokeStyle = Player1.color;
+        for (i = 0; i < Player1.trace.length; i++) {
+            mapctx.beginPath();
+            for (j = 0; j < Player1.trace[i].length; j++) {
+                mapctx.lineTo(Player1.trace[i][j][0], Player1.trace[i][j][1]);
+            }
+            mapctx.stroke();
+        }
+        mapctx.strokeStyle = "#fff";
+        mapctx.lineWidth = 8;
+        for (i = 0; i < Player1.trace.length; i++) {
+            mapctx.beginPath();
+            for (j = 0; j < Player1.trace[i].length; j++) {
+                mapctx.lineTo(Player1.trace[i][j][0], Player1.trace[i][j][1]);
+            }
+            mapctx.stroke();
+        }
+        mapctx.restore();
         
+
+
+        ctx.drawImage(mapcanvas, 0, 0);
+
+        ctx.save();
+        ctx.translate(width*0.5, height*0.7)
+        ctx.rotate(-Player1.sa * Math.PI);
+        ctx.translate(-Player1.x, -Player1.y)
+
+        // elements
+        
+        // let bg_pattern = ctx.createPattern(bg_tile, "repeat");
+        
+        
+        
+        // let map_pattern = ctx.createPattern(mapcanvas, "repeat");
+        // ctx.fillStyle = map_pattern;
+        // ctx.fillRect(-7000, -7000, 14000, 14000);
+        
+
+        draw_at_r(Player1.x, Player1.y, Player1.a, () => {
+            ctx.fillStyle = Player1.color;
+            ctx.beginPath();
+            ctx.moveTo(-16, 20);
+            ctx.quadraticCurveTo(-16, 0, 0, -20);
+            ctx.quadraticCurveTo(16, 0, 16, 20);
+            ctx.closePath();
+            ctx.fill();
+        });
         ctx.restore();
+        
+        
+        ctx.save();
         ctx.font = "16px monospace";
         ctx.strokeStyle = "#ffffff";
         ctx.strokeText(frameCount, 10, 10+16);
@@ -159,17 +257,19 @@ window.addEventListener("load", (event) => {
         ctx.font = "16px monospace";
         ctx.fillStyle = "#ffffff";
         ctx.fillText(Math.round(Player1.x * 100) / 100, 10, 10+16+16+16);
-        ctx.font = "16px monospace";
-        ctx.fillStyle = "#ffffff";
         ctx.fillText(Math.round(Player1.y * 100) / 100, 100, 10+16+16+16);
+        ctx.fillText(Math.round((Player1.a-Player1.sa) * 100) / 100, 200, 10+16+16+16);
+        ctx.restore();
     }
     document.body.addEventListener("keyup", (event) => {
         if (event.code === "ArrowLeft") {Player1.kleft = 0}
         else if (event.code === "ArrowRight") {Player1.kright = 0}
     });
     document.body.addEventListener("keydown", (event) => {
+        // console.log(event);
         if (event.code === "ArrowLeft") {Player1.kleft = -1}
         else if (event.code === "ArrowRight") {Player1.kright = 1}
+        else if (event.code === "Space") {if (pause === 0) {pause = 1;} else {pause = 0; start = undefined; next_frame();}}
     });
 
     loading();
